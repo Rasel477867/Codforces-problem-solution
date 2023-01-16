8ebf81bfd54aa4c7173436457a2ea0d210ca5286
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-16 20:00:53 +0600

    Books - solution problem: 279B
    https://codeforces.com/problemset/problem/279/B

--- code.cpp
+++ code.cpp
@@ -8,40 +8,59 @@ using namespace std;
 #define ll long long int
 #define pb push_back
 vector<ll>v;
-stack<pair<ll,ll> >s;
+set<ll>s;
+set<ll>:: iterator it;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll i,n,c,a;
-    cin>>n;
+    ll n,p,m,i,a,ans;
+    cin>>n>>m;
     for(i=0; i<n; i++)
     {
         cin>>a;
         v.pb(a);
+
     }
-    for(i=0; i<n; i++)
+    for(i=1; i<n; i++)
+        v[i]=v[i]+v[i-1];
+
+
+    p=0;
+    ll j;
+   ans=0;
+   for(j=0; j<n; j++)
+   {
+
+       if(v[j]<=m)
+        {
+            ans++;
+
+        }
+       else
+       {
+
+           break;
+       }
+   }
+    for(i=j; i<n; i++)
     {
-        while(!s.empty())
+
+
+        a=v[i]-v[p];
+
+        if(a>m)
         {
-            if(s.top().first<v[i])
-                break;
-            else
-                s.pop();
+            ans=max(i-p-1,ans);
+            p++;
         }
-        if(s.empty())
+        else
         {
-             cout<<0<<" ";
-             s.push({v[i],i+1});
+            ans=max(ans,i-p);
 
         }
-        else{
-            cout<<s.top().second<<" ";
-            s.push({v[i],i+1});
-        }
-
-
     }
-    return 0;
+
+    cout<<ans<<endl;
 }
