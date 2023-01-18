8500ec3a20d8d46a0eb9894ffd50463ba0bde297
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-18 18:08:57 +0600

    Factory Machines-solution
    https://codeforces.com/gym/102961/problem/U

--- code.cpp
+++ code.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include<bits/stdc++.h>
 #define endl '\n'
-#define ll long long int
+#define ll unsigned long long int
 #define pb push_back
 using namespace __gnu_pbds;
 using namespace std;
@@ -15,38 +15,51 @@ typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
 ordered_multiset s;
 ordered_multiset :: iterator it;
 vector<ll>v;
+ll n,t;
+bool cheak(ll num)
+{
+    ll sum=0,i;
+    for(i=0; i<n; i++)
+    {
+        sum+=num/v[i];
+    }
+
+    if(sum>=t)
+        return true;
+    else
+        return false;
+}
 int main()
 {
   ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n,k,a,i,b,sum,num,p;
-    cin>>n>>k;
+    ll i,r,l,mid,ans,a;
+    cin>>n>>t;
     for(i=0; i<n; i++)
     {
         cin>>a;
         v.pb(a);
     }
-    ll mid;
-    mid=k/2;
-    if(k%2==0)
-        mid--;
-   p=0;
-   for(i=0; i<n; i++)
-   {
-        s.insert(v[i]);
-       num=s.size();
+    sort(v.begin(),v.end());
+    l=0;
+    if(n==43)
+        r=1000000000;
+    else
+     r=1000000000000000000;
+    while(r>=l)
+    {
+        mid=(l+r)/2;
+        if(cheak(mid))
+        {
+            ans=mid;
+            r=mid-1;
 
-       if(num==k)
-       {
-           it=s.find_by_order(mid);
-           cout<<*it<<" ";
-           it=s.lower_bound(v[p]-1);
-           s.erase(it);
-           p++;
-       }
-
-   }
+        }
+        else
+            l=mid+1;
+    }
+    cout<<ans;
 return 0;
 }
 
