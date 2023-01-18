19446bcc451fa6135f208f4b7683825384c5313e
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-18 08:51:08 +0600

    Silding Median -Solution(searching and sorting)
    https://codeforces.com/gym/102961/problem/ZF

--- code.cpp
+++ code.cpp
@@ -1,33 +1,52 @@
-
-
-
-
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
 #include<bits/stdc++.h>
-using namespace std;
 #define endl '\n'
 #define ll long long int
 #define pb push_back
-vector<ll>v;
+using namespace __gnu_pbds;
+using namespace std;
 
+typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_multiset;
+ordered_multiset s;
+ordered_multiset :: iterator it;
+vector<ll>v;
 int main()
 {
-    ios::sync_with_stdio(0);
+  ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll i,n,a,b,sum=0,sum1=0,c=0;
-    cin>>n;
+    ll n,k,a,i,b,sum,num,p;
+    cin>>n>>k;
     for(i=0; i<n; i++)
     {
         cin>>a;
-        sum+=a;
         v.pb(a);
     }
-    for(i=0; i<n-1; i++)
-    {
-        sum1+=v[i];
-        if(2*sum1==sum)
-            c++;
-    }
-    cout<<c;
-    return 0;
+    ll mid;
+    mid=k/2;
+    if(k%2==0)
+        mid--;
+   p=0;
+   for(i=0; i<n; i++)
+   {
+        s.insert(v[i]);
+       num=s.size();
+
+       if(num==k)
+       {
+           it=s.find_by_order(mid);
+           cout<<*it<<" ";
+           it=s.lower_bound(v[p]-1);
+           s.erase(it);
+           p++;
+       }
+
+   }
+return 0;
 }
+
