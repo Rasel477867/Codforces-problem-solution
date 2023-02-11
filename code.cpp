721046c481c510ed144230eaeec5097eb7ea7752
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-02-11 04:19:27 +0000

    C. Two Teams Composing-solution
    https://codeforces.com/problemset/problem/1335/C

--- code.cpp
+++ code.cpp
@@ -22,101 +22,105 @@ const ll z=1e5+3;
 const ll N=1e1;
  bool sive[N];
  vector<ll>prime;
+ vector<ll>v2;
  vector<ll>v;
+ set<ll>s1;
+ set<ll>s2;
  map<ll,ll >m;
+ map<ll,ll>m1;
  map<ll,ll>:: iterator it;
 
-// ll digit(ll a)
-// {
-//     ll sum1=0;
-//     while(a!=0)
-//     {
-//         sum1+=a%10;
-//         a=a/10;
-//     }
-//     return sum1;
-// }
-// long long int gcd(long long int a,long long int b)
-//{
-//    long long int c;
-//    while(a%b!=0)
-//    {
-//        c=b;
-//        b=a%b;
-//        a=c;
-//    }
-//    return b;
-//}
-//
-//
-//
-// ll ex(ll a,ll b,ll mod)
-//{
-//    if(b==0)
-//        return 1;
-//    else if(b%2==0)
-//        return (ex((a*a)%mod,b/2,mod));
-//    else
-//        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
-//}
-// void segsive(ll l,ll r)
-// {    ll base,i,j;
-//     bool sprime[r-l+1];
-//     for(i=0; i<r-l+1; i++)
-//          sprime[i]=true;
-//     for(i=0; prime[i]*prime[i]<=r; i++)
-//     {
-//         ll cp=prime[i];
-//         base=(l/cp)*cp;
-//         if(base<l)
-//            base+=cp;
-//         for(j=base; j<=r; j+=cp)
-//              sprime[j-l]=false;
-//         if(cp==base)
-//            sprime[base-l]=true;
-//     }
-//     for(i=0; i<(r-l+1); i++)
-//       {
-//           if(sprime[i]==true)
-//            {
-//                if(i+l==1)
-//                    continue;
-//                cout<<i+l<<endl;
-//            }
-//       }
-//       cout<<endl;
-// }
-//  ll euler_pi_function(ll n)
-// { ll num,num1,num2,i;
-//      num=n;
-//     num1=n;
-//    for(i=0; prime[i]*prime[i]<=num ; i++)
-//    {
-//        while(1)
-//        {
-//            if(n%prime[i]!=0)
-//                break;
-//            else{
-//                n/=prime[i];
-//                m[prime[i]]++;
-//            }
-//        }
-//    }
-//    if(n>1)
-//        m[n]++;
-//
-//
-//       num=1;
-//      num2=1;
-//     for(it=m.begin(); it!=m.end(); it++)
-//     {
-//         num=num*(it->first);
-//         num2=num2*((it->first)-1);
-//     }
-//     num=num1/num;
-//     num=num*num2;
-//   return num;
-// }
+ ll digit(ll a)
+ {
+     ll sum1=0;
+     while(a!=0)
+     {
+         sum1+=a%10;
+         a=a/10;
+     }
+     return sum1;
+ }
+ long long int gcd(long long int a,long long int b)
+{
+    long long int c;
+    while(a%b!=0)
+    {
+        c=b;
+        b=a%b;
+        a=c;
+    }
+    return b;
+}
+
+
+
+ ll ex(ll a,ll b,ll mod)
+{
+    if(b==0)
+        return 1;
+    else if(b%2==0)
+        return (ex((a*a)%mod,b/2,mod));
+    else
+        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
+}
+ void segsive(ll l,ll r)
+ {    ll base,i,j;
+     bool sprime[r-l+1];
+     for(i=0; i<r-l+1; i++)
+          sprime[i]=true;
+     for(i=0; prime[i]*prime[i]<=r; i++)
+     {
+         ll cp=prime[i];
+         base=(l/cp)*cp;
+         if(base<l)
+            base+=cp;
+         for(j=base; j<=r; j+=cp)
+              sprime[j-l]=false;
+         if(cp==base)
+            sprime[base-l]=true;
+     }
+     for(i=0; i<(r-l+1); i++)
+       {
+           if(sprime[i]==true)
+            {
+                if(i+l==1)
+                    continue;
+                cout<<i+l<<endl;
+            }
+       }
+       cout<<endl;
+ }
+  ll euler_pi_function(ll n)
+ { ll num,num1,num2,i;
+      num=n;
+     num1=n;
+    for(i=0; prime[i]*prime[i]<=num ; i++)
+    {
+        while(1)
+        {
+            if(n%prime[i]!=0)
+                break;
+            else{
+                n/=prime[i];
+                m[prime[i]]++;
+            }
+        }
+    }
+    if(n>1)
+        m[n]++;
+
+
+       num=1;
+      num2=1;
+     for(it=m.begin(); it!=m.end(); it++)
+     {
+         num=num*(it->first);
+         num2=num2*((it->first)-1);
+     }
+     num=num1/num;
+     num=num*num2;
+   return num;
+ }
 
 int main()
 {
@@ -147,37 +151,38 @@ int main()
     }
 
     /*start main funciton*/
-   ll a,b,c,t,n,r,l,ans,mid;
-   cin>>t;
-   while(t--)
-   {
-       cin>>a>>b;
-       c=a/b;
-       if(c*b<a)
-        c++;
-       b=b*c;
-       l=1;
-       r=b;
+    ll t,n,a,ans,d,dist,s,same,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        m1.clear();
+        if(n==1)
+        {cin>>a;
+            cout<<0<<endl;
+            continue;
 
-       while(r>=l)
-       {
-           mid=(l+r)/2;
-           c=b/mid;
-           if(c*mid<b)
-             c++;
+        }
+        for(i=0; i<n; i++)
+        {
+            cin>>a;
+            m1[a]++;
+        }
+        dist=m1.size();
+        same=0;
+        for(it=m1.begin(); it!=m1.end(); it++)
+        {
+            same=max(same,it->second);
+        }
+        d=max(same,dist);
+        s=min(same,dist);
+        d--;
+        ans=min(d,s);
+        cout<<ans<<endl;
 
-           if(c<=a)
-           {
-               ans=mid;
-               r=mid-1;
-           }
-           else
-              l=mid+1;
-       }
-       cout<<ans<<endl;
 
-   }
 
+    }
 
 
 return 0;
