aea6cd8d668d8e1ea5b9440eeeaa78c6e9b85167
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-05-30 12:35:17 +0600

    D. Line-codeforce problem solution
    https://codeforces.com/contest/1722/problem/D

--- code.cpp
+++ code.cpp
@@ -16,63 +16,109 @@ typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
         ordered_multiset;
 //ordered_multiset s3;
 //ordered_multiset :: iterator it;
-const ll mod=1e9+7;
-
+const ll mod=1e9;
+ll comb[1007][1007];
 const ll N=1e1;
 bool sive[N];
 vector<ll>prime;
+ll fact[N];
 vector<ll>v;
-vector<ll>p;
-set<ll>s;
-set<ll>:: iterator it;
-ll k,num,m;
 
-long long int gcd(long long int a,long long int b)
+int convert(string s)
 {
-    long long int c;
-    while(a%b!=0)
-    {
-        c=b;
-        b=a%b;
-        a=c;
-    }
-    return b;
+    int a,b;
+    a=s[0]-48;
+    b=s[1]-48;
+    return (a*10+b);
 }
-double cal(double x)
+ll calc(ll n)
 {
-    double y=sqrt(x);
-    return(x*x+2*y);
+    ll sum=0,a,i;
+    a=n/2;
+    sum=0;
+    if(n%2==0)
+        a--;
+    for(i=n-1; i>a; i--)
+        sum+=i;
+    sum=sum*2;
+    if(n%2==1)
+        return(sum+n/2);
+     return sum;
 }
-bool cheak(ll n)
-{
-    ll b,c,a;
-    if(n>=k)
-    {
-        a=(((n*(n+1))/2)-((n-k)*((n-k)+1)/2));
-    }
-    else{
-        a=((n*(n+1))/2)+k-n;
-    }
-    ll n1=n-1;
-    c=num-k;
-    if(c<=n1)
-    {
-        b=(((n1*(n1+1))/2)-(((n1-c)*((n1-c)+1))/2));
-    }
-    else{
-
-        b=((n1*(n1+1))/2)+c-n1;
-
-    }
-
-
-    if(a+b<=m)
-        return true;
-    else
-        return false;
-
-}
-
+//string cal(string s1,string s2,int x)
+//{
+//    ll h,m,c,d,a,b;
+//    string a1,b1;
+//    char ch;
+//    h=convert(s1);
+//    m=convert(s2);
+//    c=x/60;
+//    h+=c;
+//    d=x-c*60;
+//    m+=d;
+//    h+=m/60;
+//    m=m%60;
+//    if(h>=24)
+//    {
+//        h=0;
+//        k++;
+//    }
+//    if(m<10)
+//    {
+//        ch=m+48;
+//        b1.pb('0');
+//        b1.pb(ch);
+//
+//    }
+//    else
+//    {
+//        a=m%10;
+//        ch=a+48;
+//        m=m/10;
+//        b1.pb(m+48);
+//        b1.pb(ch);
+//    }
+//    if(h<10)
+//    {
+//        ch=h+48;
+//        a1.pb('0');
+//        a1.pb(ch);
+//    }
+//    else
+//    {
+//        a=h%10;
+//        ch=a+48;
+//        h=h/10;
+//        a1.pb(h+48);
+//        a1.pb(ch);
+//    }
+//    return(a1+":"+b1);
+//
+//}
+//
+//
+//long long int gcd(long long int a,long long int b)
+//{
+//    long long int c;
+//    while(a%b!=0)
+//    {
+//        c=b;
+//        b=a%b;
+//        a=c;
+//    }
+//    return b;
+//}
+//
+//
+//ll ex(ll a,ll b,ll mod)
+//{
+//    if(b==0)
+//        return 1;
+//    else if(b%2==0)
+//        return (ex((a*a)%mod,b/2,mod));
+//    else
+//        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
+//}
 
 int main()
 {
@@ -80,53 +126,110 @@ int main()
     cin.tie(0);
     cout.tie(0);
     ll i,j;
-    for(i=0; i<N; i++)
+//    for(i=0; i<N; i++)
+//    {
+//        sive[i]=true;
+//    }
+//    sive[0]=false;
+//    sive[1]=false;
+//    for(i=2; i*i<=N; i++)
+//    {
+//        if(sive[i])
+//        {
+//            for(j=i*i; j<N; j+=i)
+//            {
+//                sive[j]=false;
+//            }
+//        }
+//    }
+//    for(i=0; i<N; i++)
+//    {
+//        if(sive[i]==true)
+//            prime.pb(i);
+//    }
+    for(i=0; i<=1005; i++)
     {
-        sive[i]=true;
-    }
-    sive[0]=false;
-    sive[1]=false;
-    for(i=2; i*i<=N; i++)
-    {
-        if(sive[i])
+        for(j=0; j<=i; j++)
         {
-            for(j=i*i; j<N; j+=i)
+            if(i==j)
+                comb[i][j]=1;
+            else if(j==0)
+                comb[i][j]=1;
+            else
+                comb[i][j]=((comb[i-1][j-1])%mod+comb[i-1][j]%mod)%mod;
+        }
+    }
+
+    fact[0]=1;
+    fact[1]=1;
+    for(i=1; i<N; i++)
+    {
+        fact[i]=(((fact[i-1])%mod)*(i%mod))%mod;
+    }
+
+    //start main funciton
+    ll t,n,a,b,ans,sum,l,r;
+    string s;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>s;
+        v.clear();
+        sum=calc(n);
+        ans=0;
+        for(i=0; i<n/2; i++)
+        {
+            if(s[i]=='L')
             {
-                sive[j]=false;
+                a=n-i-1-i;
+                ans+=a;
+                v.pb(a);
+
+
+            }
+            else
+                {
+
+                    v.pb(0);
+                }
+        }
+        b=n/2;
+        if(n%2==1)
+            b++;
+        for(i=b; i<n; i++)
+        {
+            if(s[i]=='R')
+            {
+                a=i-(n-i-1);
+                ans+=a;
+                v.pb(a);
+
+            }
+            else
+            {
+
+                v.pb(0);
             }
         }
-    }
-    for(i=0; i<N; i++)
-    {
-        if(sive[i]==true)
-            prime.pb(i);
-    }
+        if(n%2==1)
+            v.pb(0);
+        ans=sum-ans;
 
-    /*start main funciton*/
-    ll ans=0,mid,a,b,l,r;
-
-    cin>>num>>m>>k;
-    l=1;
-    r=1e9;
-
-
-    while(r>=l)
-    {
-        mid=(l+r)/2;
-        if(cheak(mid))
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        for(i=0; i<n; i++)
         {
-            ans=mid;
-            l=mid+1;
+            ans+=v[i];
+            cout<<ans<<" ";
         }
-        else{
-            r=mid-1;
-        }
+        cout<<endl;
+
     }
 
 
-   cout<<ans;
+
 
 
     return 0;
-}
 
+}
