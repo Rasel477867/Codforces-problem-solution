a5bda6ba49282e54d23f5130b127e95acf39e4f2
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-08-19 09:15:25 +0600

    B. String LCM-codeforces-solution
    https://codeforces.com/problemset/problem/1473/B

--- code.cpp
+++ code.cpp
@@ -11,6 +11,38 @@
 const ll mod=1e9+7;
 using namespace std;
 vector<ll>v;
+ string sub;
+bool cheak(ll n,string s,string s1)
+{
+    set<string>s2;
+
+    ll i;
+    if(s.size()%n!=0 || s.size()<n)
+        return false;
+    for(i=0; i<s.size()-n+1; i=i+n)
+    {
+        sub=s.substr(i,n);
+
+       if(s2.size()>1)
+          return false;
+        s2.insert(sub);
+
+    }
+    if(s1.size()%n!=0 || s1.size()<n)
+        return false;
+    for(i=0; i<s1.size()-n+1; i+=n)
+    {
+        sub=s1.substr(i,n);
+
+        if(s2.size()>1)
+            return false;
+        s2.insert(sub);
+    }
+    if(s2.size()==1)
+        return true;
+    else
+        return false;
+}
 //ll ex(ll a,ll b,ll mod)
 //{
 //    if(b==0)
@@ -20,17 +52,17 @@ vector<ll>v;
 //    else
 //        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
 //}
-//long long int gcd(long long int a,long long int b)
-//{
-//    long long int c;
-//    while(a%b!=0)
-//    {
-//        c=b;
-//        b=a%b;
-//        a=c;
-//    }
-//    return b;
-//}
+long long int gcd(long long int a,long long int b)
+{
+    long long int c;
+    while(a%b!=0)
+    {
+        c=b;
+        b=a%b;
+        a=c;
+    }
+    return b;
+}
 
 int main()
 {
@@ -38,29 +70,36 @@ int main()
     cin.tie(0);
     cout.tie(0);
     ll j,i,t;
-    ll n,a,b,num;
-    cin>>n;
-    num=n;
-    ll d=0,rev;
-    while(n!=0)
+    ll n,a,b,p,y;
+    string s,s1;
+    cin>>t;
+    while(t--)
     {
-        rev=n%10;
-        n=n/10;
-        d++;
+        cin>>s>>s1;
+        y=0;
+        a=s.size();
+        b=s1.size();
+        n=min(a,b);
+        for(i=0; i<n; i++)
+        {
+            if(cheak(i+1,s,s1))
+            {
+                p=i+1;
+                y=1;
+                break;
+            }
+        }
+        if(y==0)
+            cout<<-1<<endl;
+        else
+            {
+              a=gcd(s.size(),s1.size());
+              b=(s.size()*s1.size())/a;
+              n=sub.size();
+              for(i=1; i<=b;i+=n)
+                cout<<sub;
+              cout<<endl;
+            }
     }
-    if(rev==9)
-    {
-        a=1;
-    }
-    else{
-        a=rev+1;
-        d--;
-    }
-    n=a;
-    for(i=0; i<d; i++)
-    {
-        n=n*10;
-    }
-    cout<<n-num;
     return 0;
 }
