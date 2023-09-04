27b4e9cad03836df612bd3edf239eb2cecef4dae
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-09-04 07:48:27 +0600

    B. Binary Removals
    https://codeforces.com/contest/1499/problem/B

--- code.cpp
+++ code.cpp
@@ -10,63 +10,98 @@
 #define pf push_front
 const ll mod=1e9+7;
 using namespace std;
-
-vector<ll>v;
-vector<ll>v1;
-map<string,ll>m;
-map<string,ll>:: iterator it;
+deque<char>v;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll t,i,j;
-    ll n,k,p,a,mn=0,mx=0,c;
-    cin>>n>>k;
-    p=k;
-    for(i=0; i<n; i++)
+    ll i,j;
+    ll n,t,a,b,y;
+    string s;
+    cin>>t;
+    char ch;
+    while(t--)
     {
-        cin>>a;
-        v.pb(a);
-    }
-    sort(v.begin(),v.end());
-    while(k--)
-    {
-        string s;
         cin>>s;
-        m[s]++;
+        n=s.size();
+        v.clear();
+        for(i=0; i<n; i++)
+        {
+            v.pb(s[i]);
+        }
+        for(i=0; i<n; i++)
+        {
+            if(v.front()=='0')
+                v.pop_front();
+            else
+               {
+                    ch=v.front();
+                   v.pop_front();
+                   if(v.front()=='0')
+                     continue;
+                   else{
+                    v.pf(ch);
+                   }
+               }
+        }
+        if(v.size()==0)
+            cout<<"YES"<<endl;
+        else
+        {
+            for(i=0; i<v.size(); i++)
+            {
+                if(v.back()=='1')
+                    v.pop_back();
+                else
+                    {
+                        ch=v.back();
+                       v.pop_back();
+                       if(v.back()=='1')
+                       {
+                           continue;
+                       }
+                       else{
+                        v.pb(ch);
+                       }
+                    }
+            }
+            if(v.size()==0)
+                cout<<"YES"<<endl;
+            else
+            {
+                y=0;
+                v.pb('1');
+                for(i=0; i<v.size()-1; i++)
+                {
+                    if(v[i]=='0' && v[i+1]=='0')
+                    {
+                        y=1;
+                        break;
+                    }
+                }
+                if(y==0)
+                    cout<<"YES"<<endl;
+                else
+                {
+                    y=0;
+                    for(i=0; i<v.size()-1; i++)
+                    {
+                        if(v[i]=='1' && v[i+1]=='1')
+                        {
+                            y=1;
+                            break;
+                        }
+                    }
+                    if(y==0)
+                        cout<<"YES"<<endl;
+                    else
+                        cout<<"NO"<<endl;
+                }
+            }
+        }
     }
-    for(it=m.begin(); it!=m.end(); it++)
-    {
-        v1.pb(it->second);
-    }
-    sort(v1.begin(),v1.end());
-    c=0;
-    reverse(v1.begin(),v1.end());
 
-    for(i=0; i<n; i++)
-    {
-        if(c==p)
-            break;
-        c+=v1[i];
-        mn+=v[i]*v1[i];
-        if(c==p)
-            break;
-    }
-    reverse(v.begin(),v.end());
-    c=0;
-    for(i=0; i<n; i++)
-    {
-        if(c==p)
-            break;
-        c+=v1[i];
-        mx+=v[i]*v1[i];
-        if(c==p)
-            break;
-    }
-
-   cout<<mn<<" "<<mx;
-
-return 0;
+    return 0;
 
 }
